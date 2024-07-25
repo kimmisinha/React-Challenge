40fc4f17c99cd792aa19a6852125772d37974b19
kimmisinha <kimmi.sinha@newput.com>
2024-07-25 11:23:36 +0530

    Add ascending and descending sort buttons

--- my-challenge/src/Component/Question1.jsx
+++ my-challenge/src/Component/Question1.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
-import React  from 'react'
+import React from "react";
 
 function Question1() {
-  const [elves, setElves] = useState([
-    'Buddy',
-    'Dobby',
-    'Winky',
-    'Bing',
-    'Bernard'
-  ]);
+  const [elves, setElves] = useState(['Buddy', 'Dobby', 'Winky', 'Bing', 'Bernard']);
 
+   function sortAscending(){
+
+    setElves([...elves].sort());
+   }
+   function sortDescending(){
+
+    setElves([...elves].sort().reverse());
+
+   }
   return (
     <div>
-      {/* */}
-      {JSON.stringify(elves)}
+      <button onClick={sortAscending}>Sort A-Z</button>
+      <button onClick={sortDescending}>Sort Z-A</button>
+      <ul>
+        {elves && elves.map((ele, index) => (
+          <li key={index}>{ele}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default Question1
+export default Question1;
