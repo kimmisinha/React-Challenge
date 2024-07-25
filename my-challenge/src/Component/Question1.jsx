55b98567088b005320729b802a23f06278494e68
kimmisinha <kimmi.sinha@newput.com>
2024-07-25 15:10:05 +0530

    Add Animal Inventor component to combine names of two animals

--- my-challenge/src/Component/Question1.jsx
+++ my-challenge/src/Component/Question1.jsx
@@ -4,18 +4,23 @@ import React from "react";
 function Question1() {
   const [elves, setElves] = useState(['Buddy', 'Dobby', 'Winky', 'Bing', 'Bernard']);
 
-   function sortAscending(){
+  //  function sortAscending(){
 
-    setElves([...elves].sort());
-   }
+  //   setElves([...elves].sort());
+  //  }
+
    function sortDescending(){
 
-    setElves([...elves].sort().reverse());
-
+    // setElves([...elves].sort().reverse());
+    setElves((prev)=>{
+      return [...prev.sort().reverse()]
+    })
    }
+   console.log(elves)
+
   return (
     <div>
-      <button onClick={sortAscending}>Sort A-Z</button>
+      {/* <button onClick={sortAscending}>Sort A-Z</button> */}
       <button onClick={sortDescending}>Sort Z-A</button>
       <ul>
         {elves && elves.map((ele, index) => (
--- my-challenge/src/Component/Question8.jsx
+++ my-challenge/src/Component/Question8.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Question8() {
+  const animals = ['lion', 'tiger', 'bear', 'monkey', 'panda', 'zebra', 'giraffe'];
+  const [addanimal, setaddanimal] = useState("");
+
+  function AnimalInventor() {
+    const animal1 = animals[Math.floor(Math.random() * animals.length)];
+    const remainingAnimals = animals.filter(animal => animal !== animal1);
+    const animal2 = remainingAnimals[Math.floor(Math.random() * remainingAnimals.length)];
+    const combinedName = animal1.slice(0, -2) + animal2.slice(2);
+
+    // console.log("Animal1:", animal1, "Animal2:", animal2, "Combined Name:", combinedName);
+    setaddanimal(combinedName);
+  }
+
   return (
-    <div>Question8</div>
-  )
+    <div>
+      <h1>Animal Name Here</h1>
+      <p>{addanimal}</p>
+      <button onClick={AnimalInventor}>New Animal</button>
+    </div>
+  );
 }
 
-export default Question8
+export default Question8;
+
+/*
+Puzzle #8: Animal Inventor 🦜
+Create an animal inventor app that combines the names of two animals in an array. 
+To complete this task you must: Remove the last two characters of the first animal’s
+name, Remove the first two characters of the second animal’s name, Ensure that the 
+same animal name is not used twice. Example output: panbra (panda+zebra)
+ */
